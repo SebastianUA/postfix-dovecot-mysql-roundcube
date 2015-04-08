c28605aa7490f1e06550eab969767783d922e890
Vitaliy Sergeevich Natarov <solo.metal@bigmir.net>
2015-04-08 17:37:08 +0300

    added the structures for databases
    added the structures for databases

---
+++ Structures_for_DBs/iredadmin.sql
@@ -0,0 +1,107 @@
+-- MySQL dump 10.13  Distrib 5.1.73, for redhat-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: iredadmin
+-- ------------------------------------------------------
+-- Server version	5.1.73
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `deleted_mailboxes`
+--
+
+DROP TABLE IF EXISTS `deleted_mailboxes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `deleted_mailboxes` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `username` varchar(255) NOT NULL DEFAULT '',
+  `domain` varchar(255) NOT NULL DEFAULT '',
+  `maildir` varchar(255) NOT NULL DEFAULT '',
+  `admin` varchar(255) NOT NULL DEFAULT '',
+  KEY `id` (`id`),
+  KEY `timestamp` (`timestamp`),
+  KEY `username` (`username`),
+  KEY `domain` (`domain`),
+  KEY `admin` (`admin`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `log`
+--
+
+DROP TABLE IF EXISTS `log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `log` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `admin` varchar(255) NOT NULL,
+  `ip` varchar(40) NOT NULL,
+  `domain` varchar(255) NOT NULL DEFAULT '',
+  `username` varchar(255) NOT NULL DEFAULT '',
+  `event` varchar(10) NOT NULL DEFAULT '',
+  `loglevel` varchar(10) NOT NULL DEFAULT 'info',
+  `msg` varchar(255) NOT NULL,
+  KEY `id` (`id`),
+  KEY `timestamp` (`timestamp`),
+  KEY `admin` (`admin`),
+  KEY `ip` (`ip`),
+  KEY `domain` (`domain`),
+  KEY `username` (`username`),
+  KEY `event` (`event`),
+  KEY `loglevel` (`loglevel`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `sessions`
+--
+
+DROP TABLE IF EXISTS `sessions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sessions` (
+  `session_id` char(128) NOT NULL,
+  `atime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `data` text,
+  UNIQUE KEY `session_id` (`session_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `updatelog`
+--
+
+DROP TABLE IF EXISTS `updatelog`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `updatelog` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  KEY `id` (`id`),
+  KEY `date` (`date`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-04-08 17:05:11
---
+++ Structures_for_DBs/vmail.sql
@@ -0,0 +1,409 @@
+-- MySQL dump 10.13  Distrib 5.1.73, for redhat-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: vmail
+-- ------------------------------------------------------
+-- Server version	5.1.73
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `admin` (
+  `username` varchar(255) NOT NULL DEFAULT '',
+  `password` varchar(255) NOT NULL DEFAULT '',
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `language` varchar(5) NOT NULL DEFAULT 'en_US',
+  `passwordlastchange` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `settings` text,
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `expired` datetime NOT NULL DEFAULT '9999-12-31 00:00:00',
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`username`),
+  KEY `passwordlastchange` (`passwordlastchange`),
+  KEY `expired` (`expired`),
+  KEY `active` (`active`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `alias`
+--
+
+DROP TABLE IF EXISTS `alias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `alias` (
+  `address` varchar(255) NOT NULL DEFAULT '',
+  `goto` text,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `moderators` text,
+  `accesspolicy` varchar(30) NOT NULL DEFAULT '',
+  `domain` varchar(255) NOT NULL DEFAULT '',
+  `islist` tinyint(1) NOT NULL DEFAULT '0',
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `expired` datetime NOT NULL DEFAULT '9999-12-31 00:00:00',
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`address`),
+  KEY `domain` (`domain`),
+  KEY `islist` (`islist`),
+  KEY `expired` (`expired`),
+  KEY `active` (`active`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `alias_domain`
+--
+
+DROP TABLE IF EXISTS `alias_domain`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `alias_domain` (
+  `alias_domain` varchar(255) NOT NULL,
+  `target_domain` varchar(255) NOT NULL,
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`alias_domain`),
+  KEY `target_domain` (`target_domain`),
+  KEY `active` (`active`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `anyone_shares`
+--
+
+DROP TABLE IF EXISTS `anyone_shares`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `anyone_shares` (
+  `from_user` varchar(255) NOT NULL,
+  `dummy` char(1) DEFAULT '1',
+  PRIMARY KEY (`from_user`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `deleted_mailboxes`
+--
+
+DROP TABLE IF EXISTS `deleted_mailboxes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `deleted_mailboxes` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `username` varchar(255) NOT NULL DEFAULT '',
+  `domain` varchar(255) NOT NULL DEFAULT '',
+  `maildir` varchar(255) NOT NULL DEFAULT '',
+  `admin` varchar(255) NOT NULL DEFAULT '',
+  KEY `id` (`id`),
+  KEY `timestamp` (`timestamp`),
+  KEY `username` (`username`),
+  KEY `domain` (`domain`),
+  KEY `admin` (`admin`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `domain`
+--
+
+DROP TABLE IF EXISTS `domain`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `domain` (
+  `domain` varchar(255) NOT NULL DEFAULT '',
+  `description` text,
+  `disclaimer` text,
+  `aliases` int(10) NOT NULL DEFAULT '0',
+  `mailboxes` int(10) NOT NULL DEFAULT '0',
+  `maxquota` bigint(20) NOT NULL DEFAULT '0',
+  `quota` bigint(20) NOT NULL DEFAULT '0',
+  `transport` varchar(255) NOT NULL DEFAULT 'dovecot',
+  `backupmx` tinyint(1) NOT NULL DEFAULT '0',
+  `settings` text,
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `expired` datetime NOT NULL DEFAULT '9999-12-31 00:00:00',
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`domain`),
+  KEY `backupmx` (`backupmx`),
+  KEY `expired` (`expired`),
+  KEY `active` (`active`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `domain_admins`
+--
+
+DROP TABLE IF EXISTS `domain_admins`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `domain_admins` (
+  `username` varchar(255) CHARACTER SET ascii NOT NULL DEFAULT '',
+  `domain` varchar(255) CHARACTER SET ascii NOT NULL DEFAULT '',
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `expired` datetime NOT NULL DEFAULT '9999-12-31 00:00:00',
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`username`,`domain`),
+  KEY `username` (`username`),
+  KEY `domain` (`domain`),
+  KEY `active` (`active`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `mailbox`
+--
+
+DROP TABLE IF EXISTS `mailbox`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mailbox` (
+  `username` varchar(255) NOT NULL DEFAULT '',
+  `password` varchar(255) NOT NULL DEFAULT '',
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `language` varchar(5) NOT NULL DEFAULT 'en_US',
+  `storagebasedirectory` varchar(255) NOT NULL DEFAULT '/var/vmail',
+  `storagenode` varchar(255) NOT NULL DEFAULT 'vmail1',
+  `maildir` varchar(255) NOT NULL DEFAULT '',
+  `quota` bigint(20) NOT NULL DEFAULT '0',
+  `domain` varchar(255) NOT NULL DEFAULT '',
+  `transport` varchar(255) NOT NULL DEFAULT '',
+  `department` varchar(255) NOT NULL DEFAULT '',
+  `rank` varchar(255) NOT NULL DEFAULT 'normal',
+  `employeeid` varchar(255) DEFAULT '',
+  `isadmin` tinyint(1) NOT NULL DEFAULT '0',
+  `isglobaladmin` tinyint(1) NOT NULL DEFAULT '0',
+  `enablesmtp` tinyint(1) NOT NULL DEFAULT '1',
+  `enablesmtpsecured` tinyint(1) NOT NULL DEFAULT '1',
+  `enablepop3` tinyint(1) NOT NULL DEFAULT '1',
+  `enablepop3secured` tinyint(1) NOT NULL DEFAULT '1',
+  `enableimap` tinyint(1) NOT NULL DEFAULT '1',
+  `enableimapsecured` tinyint(1) NOT NULL DEFAULT '1',
+  `enabledeliver` tinyint(1) NOT NULL DEFAULT '1',
+  `enablelda` tinyint(1) NOT NULL DEFAULT '1',
+  `enablemanagesieve` tinyint(1) NOT NULL DEFAULT '1',
+  `enablemanagesievesecured` tinyint(1) NOT NULL DEFAULT '1',
+  `enablesieve` tinyint(1) NOT NULL DEFAULT '1',
+  `enablesievesecured` tinyint(1) NOT NULL DEFAULT '1',
+  `enableinternal` tinyint(1) NOT NULL DEFAULT '1',
+  `enabledoveadm` tinyint(1) NOT NULL DEFAULT '1',
+  `enablelib-storage` tinyint(1) NOT NULL DEFAULT '1',
+  `enableindexer-worker` tinyint(1) NOT NULL DEFAULT '1',
+  `enablelmtp` tinyint(1) NOT NULL DEFAULT '1',
+  `enabledsync` tinyint(1) NOT NULL DEFAULT '1',
+  `lastlogindate` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `lastloginipv4` int(4) unsigned NOT NULL DEFAULT '0',
+  `lastloginprotocol` char(255) NOT NULL DEFAULT '',
+  `disclaimer` text,
+  `allowedsenders` text,
+  `rejectedsenders` text,
+  `allowedrecipients` text,
+  `rejectedrecipients` text,
+  `settings` text,
+  `passwordlastchange` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `expired` datetime NOT NULL DEFAULT '9999-12-31 00:00:00',
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  `local_part` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`username`),
+  KEY `domain` (`domain`),
+  KEY `department` (`department`),
+  KEY `employeeid` (`employeeid`),
+  KEY `isadmin` (`isadmin`),
+  KEY `isglobaladmin` (`isglobaladmin`),
+  KEY `enablesmtp` (`enablesmtp`),
+  KEY `enablesmtpsecured` (`enablesmtpsecured`),
+  KEY `enablepop3` (`enablepop3`),
+  KEY `enablepop3secured` (`enablepop3secured`),
+  KEY `enableimap` (`enableimap`),
+  KEY `enableimapsecured` (`enableimapsecured`),
+  KEY `enabledeliver` (`enabledeliver`),
+  KEY `enablelda` (`enablelda`),
+  KEY `enablemanagesieve` (`enablemanagesieve`),
+  KEY `enablemanagesievesecured` (`enablemanagesievesecured`),
+  KEY `enablesieve` (`enablesieve`),
+  KEY `enablesievesecured` (`enablesievesecured`),
+  KEY `enablelmtp` (`enablelmtp`),
+  KEY `enableinternal` (`enableinternal`),
+  KEY `enabledoveadm` (`enabledoveadm`),
+  KEY `enablelib-storage` (`enablelib-storage`),
+  KEY `enableindexer-worker` (`enableindexer-worker`),
+  KEY `enabledsync` (`enabledsync`),
+  KEY `passwordlastchange` (`passwordlastchange`),
+  KEY `expired` (`expired`),
+  KEY `active` (`active`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `recipient_bcc_domain`
+--
+
+DROP TABLE IF EXISTS `recipient_bcc_domain`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `recipient_bcc_domain` (
+  `domain` varchar(255) NOT NULL DEFAULT '',
+  `bcc_address` varchar(255) NOT NULL DEFAULT '',
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `expired` datetime NOT NULL DEFAULT '9999-12-31 00:00:00',
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`domain`),
+  KEY `bcc_address` (`bcc_address`),
+  KEY `expired` (`expired`),
+  KEY `active` (`active`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `recipient_bcc_user`
+--
+
+DROP TABLE IF EXISTS `recipient_bcc_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `recipient_bcc_user` (
+  `username` varchar(255) NOT NULL DEFAULT '',
+  `bcc_address` varchar(255) NOT NULL DEFAULT '',
+  `domain` varchar(255) NOT NULL DEFAULT '',
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `expired` datetime NOT NULL DEFAULT '9999-12-31 00:00:00',
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`username`),
+  KEY `bcc_address` (`bcc_address`),
+  KEY `expired` (`expired`),
+  KEY `active` (`active`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `sender_bcc_domain`
+--
+
+DROP TABLE IF EXISTS `sender_bcc_domain`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sender_bcc_domain` (
+  `domain` varchar(255) NOT NULL DEFAULT '',
+  `bcc_address` varchar(255) NOT NULL DEFAULT '',
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `expired` datetime NOT NULL DEFAULT '9999-12-31 00:00:00',
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`domain`),
+  KEY `bcc_address` (`bcc_address`),
+  KEY `expired` (`expired`),
+  KEY `active` (`active`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `sender_bcc_user`
+--
+
+DROP TABLE IF EXISTS `sender_bcc_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sender_bcc_user` (
+  `username` varchar(255) NOT NULL DEFAULT '',
+  `bcc_address` varchar(255) NOT NULL DEFAULT '',
+  `domain` varchar(255) NOT NULL DEFAULT '',
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `expired` datetime NOT NULL DEFAULT '9999-12-31 00:00:00',
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`username`),
+  KEY `bcc_address` (`bcc_address`),
+  KEY `domain` (`domain`),
+  KEY `expired` (`expired`),
+  KEY `active` (`active`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `share_folder`
+--
+
+DROP TABLE IF EXISTS `share_folder`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `share_folder` (
+  `from_user` varchar(255) CHARACTER SET ascii NOT NULL,
+  `to_user` varchar(255) CHARACTER SET ascii NOT NULL,
+  `dummy` char(1) DEFAULT NULL,
+  PRIMARY KEY (`from_user`,`to_user`),
+  KEY `from_user` (`from_user`),
+  KEY `to_user` (`to_user`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `used_quota`
+--
+
+DROP TABLE IF EXISTS `used_quota`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `used_quota` (
+  `username` varchar(255) NOT NULL,
+  `bytes` bigint(20) NOT NULL DEFAULT '0',
+  `messages` bigint(20) NOT NULL DEFAULT '0',
+  `domain` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`username`),
+  KEY `domain` (`domain`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = latin1 */ ;
+/*!50003 SET character_set_results = latin1 */ ;
+/*!50003 SET collation_connection  = latin1_swedish_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = '' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `after_insert_used_quota` BEFORE INSERT ON `used_quota` FOR EACH ROW
+    BEGIN
+        SET NEW.domain = SUBSTRING_INDEX(NEW.username, '@', -1);
+    END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-04-08 17:06:06

11112250b25317fe632a67751ff7c66b90cedb04
Vitaliy Sergeevich Natarov <solo.metal@bigmir.net>
2015-04-09 18:27:46 +0300

    Update install-Debians.sh

--- install-Debians.sh
+++ install-Debians.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
-apt-get install git wget 
+#delete sendmail:
+apt-get remove sendmail
+
+#Install some utilits: 
+apt-get install mlocate bind-utils telnet mailx sharutils
+if ! type -path "wget" > /dev/null 2>&1; then apt-get install wget -y; fi
+if ! type -path "git" > /dev/null 2>&1; then apt-get install git -y; fi
+if ! type -path "httpd" > /dev/null 2>&1; then apt-get install httpd -y; fi
+if ! type -path "php" > /dev/null 2>&1; then apt-get install php -y; fi
+if ! type -path "mysql" > /dev/null 2>&1; then apt-get install mysql -y; fi
+if ! type -path "postfix" > /dev/null 2>&1; then apt-get install postfix -y; fi
+if ! type -path "dovecot" > /dev/null 2>&1; then apt-get install dovecot dovecot-mysql -y; fi
+
+
 cd /usr/local/src && git clone https://github.com/SebastianUA/postfix-dovecot-mysql-roundcube.git
 
 #moving to etc
@@ -12,5 +25,26 @@ mv postfix-dovecot-mysql-roundcube/mysql/* /etc/
 #moving to /var/www/
 mv postfix-dovecot-mysql-roundcube/postfix/roundcubemail /var/www/
 mv postfix-dovecot-mysql-roundcube/postfix/iredadmin /var/www/
+
+
+#restore DBs
+mysql -uroot -p
+
+CREATE database iredadmin;
+GRANT ALL ON iredadmin.* TO iredadmin@localhost IDENTIFIED BY 'iredadmin_pw';
+CREATE database roundcube;
+GRANT ALL ON roundcube.* TO roundcube@localhost IDENTIFIED BY 'roundcube_pw';
+CREATE database vmail;
+GRANT ALL ON vmail.* TO vmail@localhost IDENTIFIED BY 'vmail_pw';
+GRANT ALL ON vmail.* TO vmailadmin@localhost IDENTIFIED BY 'vmailadmin_pw';
+exit;
+
+
+#remove trash
+rm -rf /usr/local/src/postfix-dovecot-mysql-roundcube
+echo "================================================";
+echo "DONE!";
+echo "================================================";
+
 
 

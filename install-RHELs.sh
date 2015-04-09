960ad6f9eadc8dde84936f299c298d38414ac39d
Vitaliy Sergeevich Natarov <solo.metal@bigmir.net>
2015-04-09 18:21:23 +0300

    Update install-RHELs.sh

--- install-RHELs.sh
+++ install-RHELs.sh
@@ -30,5 +30,18 @@ mv postfix-dovecot-mysql-roundcube/postfix/iredadmin /var/www/
 #restore DBs
 mysql -uroot -p
 
+CREATE database iredadmin;
+GRANT ALL ON iredadmin.* TO iredadmin@localhost IDENTIFIED BY 'iredadmin_pw’;
+CREATE database roundcube;
+GRANT ALL ON roundcube.* TO roundcube@localhost IDENTIFIED BY 'roundcube_pw';
+CREATE database vmail;
+GRANT ALL ON vmail.* TO vmail@localhost IDENTIFIED BY 'vmail'_pw;
+GRANT ALL ON vmail.* TO vmailadmin@localhost IDENTIFIED BY 'vmailadmin_pw';
+exit;
 
 
+#remove trash
+rm -rf /usr/local/src/postfix-dovecot-mysql-roundcube
+echo "================================================";
+echo "DONE!";
+echo "================================================";

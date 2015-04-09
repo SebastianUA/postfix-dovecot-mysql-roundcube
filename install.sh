aaf309db39e521ac8a099bb88e40db63f5814272
Vitaliy Sergeevich Natarov <solo.metal@bigmir.net>
2015-04-09 18:01:12 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin
+
+cd /usr/local/src && git clone https://github.com/SebastianUA/postfix-dovecot-mysql-roundcube.git
+
+#moving to etc
+mv postfix-dovecot-mysql-roundcube/dovecot /etc/
+mv postfix-dovecot-mysql-roundcube/httpd /etc/
+mv postfix-dovecot-mysql-roundcube/postfix /etc/
+mv postfix-dovecot-mysql-roundcube/mysql/* /etc/
+
+#moving to /var/www/
+mv postfix-dovecot-mysql-roundcube/postfix/roundcubemail /var/www/
+mv postfix-dovecot-mysql-roundcube/postfix/roundcubemail /var/www/
+
+

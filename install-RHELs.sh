e55e723563454d2444e2d8c4b673984d4164763a
Vitaliy Sergeevich Natarov <solo.metal@bigmir.net>
2015-04-09 18:39:57 +0300

    Update install-RHELs.sh
    added php php-imap php-mysql php-mbstring php-xml php-pdo php-mcrypt php-intl

--- install-RHELs.sh
+++ install-RHELs.sh
@@ -10,7 +10,7 @@ yum install mlocate bind-utils telnet mailx sharutils
 if ! type -path "wget" > /dev/null 2>&1; then yum install wget -y; fi
 if ! type -path "git" > /dev/null 2>&1; then yum install git -y; fi
 if ! type -path "httpd" > /dev/null 2>&1; then yum install httpd -y; fi
-if ! type -path "php" > /dev/null 2>&1; then yum install php -y; fi
+if ! type -path "php" > /dev/null 2>&1; then yum install php php-imap php-mysql php-mbstring php-xml php-pdo php-mcrypt php-intl -y; fi
 if ! type -path "mysql" > /dev/null 2>&1; then yum install mysql -y; fi
 if ! type -path "postfix" > /dev/null 2>&1; then yum install postfix -y; fi
 if ! type -path "dovecot" > /dev/null 2>&1; then yum install dovecot dovecot-mysql -y; fi

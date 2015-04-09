df7aac9eb707abcb836f61e07587ff9ca942d30e
Vitaliy Sergeevich Natarov <solo.metal@bigmir.net>
2015-04-09 20:26:42 +0300

    Update install-RHELs.sh

--- install-RHELs.sh
+++ install-RHELs.sh
@@ -11,7 +11,7 @@ if ! type -path "wget" > /dev/null 2>&1; then yum install wget -y; fi
 if ! type -path "git" > /dev/null 2>&1; then yum install git -y; fi
 if ! type -path "httpd" > /dev/null 2>&1; then yum install httpd -y; fi
 if ! type -path "php" > /dev/null 2>&1; then yum install php php-imap php-mysql php-mbstring php-xml php-pdo php-mcrypt php-intl -y; fi
-if ! type -path "mysql" > /dev/null 2>&1; then yum install mysql -y; mysql_secure_installation; fi
+if ! type -path "mysql" > /dev/null 2>&1; then yum install mysql mysql-server -y; mysql_secure_installation; fi
 if ! type -path "postfix" > /dev/null 2>&1; then yum install postfix -y; fi
 if ! type -path "dovecot" > /dev/null 2>&1; then yum install dovecot dovecot-mysql -y; fi
 

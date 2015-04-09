0f7549a3c9a756340cef0e77730449d540ab9af1
Vitaliy Sergeevich Natarov <solo.metal@bigmir.net>
2015-04-09 18:05:00 +0300

    Create install-RHELs.sh

---
+++ install-RHELs.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+yum install git wget 
+cd /usr/local/src && git clone https://github.com/SebastianUA/postfix-dovecot-mysql-roundcube.git
+
+#moving to etc
+mv postfix-dovecot-mysql-roundcube/dovecot /etc/
+mv postfix-dovecot-mysql-roundcube/httpd /etc/
+mv postfix-dovecot-mysql-roundcube/postfix /etc/
+mv postfix-dovecot-mysql-roundcube/mysql/* /etc/
+
+#moving to /var/www/
+mv postfix-dovecot-mysql-roundcube/postfix/roundcubemail /var/www/
+mv postfix-dovecot-mysql-roundcube/postfix/iredadmin /var/www/
+
+

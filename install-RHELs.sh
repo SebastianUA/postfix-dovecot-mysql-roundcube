e979310a4e1aa52de53bec9b6f7cae27dae84408
Vitaliy Sergeevich Natarov <solo.metal@bigmir.net>
2015-04-23 17:44:31 +0300

    Update install-RHELs.sh

--- install-RHELs.sh
+++ install-RHELs.sh
@@ -49,8 +49,8 @@ GRANT ALL ON iredadmin.* TO iredadmin@localhost IDENTIFIED BY 'iredadmin_pw';
 CREATE database roundcubemail;
 GRANT ALL ON roundcubemail.* TO roundcube@localhost IDENTIFIED BY 'roundcube_pw';
 CREATE database vmail;
-GRANT ALL ON vmail.* TO vmail@localhost IDENTIFIED BY 'vmail_pw';
-GRANT ALL ON vmail.* TO vmailadmin@localhost IDENTIFIED BY 'vmailadmin_pw';
+GRANT ALL ON vmail.* TO vmail@localhost IDENTIFIED BY 'BKG9DBgycYFbsXTH8oU9q7sLUHRCxM';
+GRANT ALL ON vmail.* TO vmailadmin@localhost IDENTIFIED BY 'BGKeeM8sm3s0KuLg2MmFJxLxGydkhc';
 flush privileges;
 exit;
 EOF
